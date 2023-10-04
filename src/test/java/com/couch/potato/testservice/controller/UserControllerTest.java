701a2d509761815cb02beefe748d19c868933944
margarita <madgista@mail.ru>
2023-10-05 00:42:31 +0300

    added tracing to the tests

--- src/test/java/com/couch/potato/testservice/controller/UserControllerTest.java
+++ src/test/java/com/couch/potato/testservice/controller/UserControllerTest.java
@@ -1,26 +1,39 @@
 package com.couch.potato.testservice.controller;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.couch.potato.testservice.data.domain.User;
 import com.couch.potato.testservice.data.repository.UserRepository;
 import com.couch.potato.testservice.dto.UserDto;
 import com.couch.potato.testservice.testcontainers.ContainerName;
 import com.couch.potato.testservice.testcontainers.EnableTestContainers;
 import io.restassured.RestAssured;
+import lombok.SneakyThrows;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 @Tag("integration-test")
+@AutoConfigureObservability
 @EnableTestContainers(ContainerName.POSTGRESQL)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "logging.level.org.apache.kafka.clients.NetworkClient=OFF")
 class UserControllerTest {
+
+    private static final ListAppender<ILoggingEvent> loggerAppender = new ListAppender<>();
+    private static final Logger logger = (Logger) LoggerFactory.getLogger("logger");
 
     @LocalServerPort
     private int port;
@@ -32,6 +45,10 @@ class UserControllerTest {
     void init() {
         repository.deleteAll();
         RestAssured.port = port;
+
+        loggerAppender.start();
+        logger.setLevel(Level.ALL);
+        logger.addAppender(loggerAppender);
     }
 
     @AfterAll
@@ -39,8 +56,13 @@ class UserControllerTest {
         RestAssured.reset();
     }
 
-    @Test
-    void activateWhenUsernameExists() {
+    @SneakyThrows
+    @ParameterizedTest
+    @ValueSource(strings = {"INFO", "ERROR", "WARN"})
+    void activateWhenUsernameExists(String logLevel) {
+        var level = Level.toLevel(logLevel);
+        logger.setLevel(level);
+
         var username = RandomStringUtils.randomAlphabetic(10);
         var user = new User();
         user.setUsername(username);
@@ -63,5 +85,9 @@ class UserControllerTest {
 
         Assertions.assertThat(userDto).isNotNull();
         repository.deleteAll();
+
+        Assertions.assertThat(loggerAppender.list).hasSize(1);
+        var event = loggerAppender.list.get(0);
+        Assertions.assertThat(event.getMDCPropertyMap()).containsKeys("traceId", "spanId");
     }
 }
